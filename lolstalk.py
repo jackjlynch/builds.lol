86250afca896895097a048987f97384713a88821
Jack Lynch <jacklynch@wharnal.com>
2015-08-10 23:00:02 -0400

    initial api test code

--- lolstalk.py
+++ lolstalk.py
@@ -1,12 +1,21 @@
 from flask import Flask
+from riotwatcher.riotwatcher import RiotWatcher
 
 app = Flask(__name__)
 
+riot_api = None
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+@app.route('/userinfo/<username>')
+def user_info(username):
+    return str(riot_api.get_summoner(name=username)['id'])
+
 
 if __name__ == '__main__':
+    app.debug = True
+    app.config.from_pyfile('dev.cfg')
+    riot_api = RiotWatcher(app.config['API_KEY'])
     app.run()
