85f81b75816a4f35194705c807c9d6cf72073e33
Jack Lynch <jacklynch@wharnal.com>
2015-08-12 23:10:41 -0400

    gets items from api

--- buildslol.py
+++ buildslol.py
@@ -5,20 +5,24 @@ from riotwatcher.riotwatcher import RiotWatcher
 app = Flask(__name__)
 
 riot_api = None
+items = None
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return app.send_static_file('index.html')
+
+@app.route('/items')
+def items():
+    return render_template('items.html', items=items['data'])
 
 @app.route('/userinfo/<username>')
 def user_info(username):
     game = riot_api.get_current_game(riot_api.get_summoner(name=username)['id'])
-    return render_template('gameinfo.html', participants=game['participants'])
-    # return str(game['participants'][0])
-
+    return render_template('items.html', participants=game['participants'])
 
 if __name__ == '__main__':
     app.debug = True
     app.config.from_pyfile('dev.cfg')
     riot_api = RiotWatcher(app.config['API_KEY'])
+    items = riot_api.static_get_item_list()
     app.run()
