eb11e283a93e24ad076870b714a5b7058db2960e
Jack Lynch <jacklynch@wharnal.com>
2015-08-16 15:50:13 -0400

    draggables in grid

--- static/items.js
+++ static/items.js
@@ -11,7 +11,7 @@ interact('.draggable')
         inertia: true,
         // keep the element within the area of it's parent
         restrict: {
-            restriction: "parent",
+            restriction: ".container",
             endOnly: true,
             elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
         },
